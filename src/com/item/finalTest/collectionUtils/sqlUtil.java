5349f7be31e89fc5bb92c912e194d7003d7f11a0
xiaohe1008 <53325710+xiaohe1008@users.noreply.github.com>
2019-09-21 01:26:07 +0800

    相关工具补充

--- src/com/item/finalTest/collectionUtils/sqlUtil.java
+++ src/com/item/finalTest/collectionUtils/sqlUtil.java
@@ -1,7 +1,9 @@
 package com.item.finalTest.collectionUtils;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 /**
  * 数据库封装类
@@ -9,20 +11,24 @@ import java.sql.DriverManager;
  * @date   2019年9月14日
  */
 public final class sqlUtil {
-
-	private static final String USER_ACCOUNT = "root";
-
-	private static final String USER_PASSWORD = "123456";
-	
-	private static final String DATE_BASE="finaltest";
-
 	public static Connection getConnect() {
+		 Properties prop = new Properties();
 		Connection conn = null;
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/"+DATE_BASE+"?useUnicode=true&characterEncoding=UTF-8&serverTimezone=GMT%2B8",
-					USER_ACCOUNT, USER_PASSWORD);
+			prop.load(new FileInputStream("src/configuration/jdbc.properties"));
+	         String driver = prop.getProperty("driver");
+	         String user=prop.getProperty("user");
+	         String password=prop.getProperty("password");
+	         String dataBase=prop.getProperty("datebase");
+	        if(!StringUtil.isBlank(driver)&&!StringUtil.isBlank(user)&&!StringUtil.isBlank(password)&&!StringUtil.isBlank(dataBase)) {
+	        	Class.forName(driver);
+				conn = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/"+dataBase+"?useUnicode=true&characterEncoding=UTF-8&serverTimezone=GMT%2B8",
+						user, password);
+	        	
+	        }else {
+	        	throw  new Exception("jdbc配置不完整，请完善"); 
+	        }
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
